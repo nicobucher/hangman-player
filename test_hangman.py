8277b27d81370963c2138d331452fe99d513cedc
Nico Bucher <nicobucher@Nicos-MacBook-Pro.local>
2018-06-28 11:39:24 +0200

    introduced more test parameters

--- test_hangman.py
+++ test_hangman.py
@@ -3,29 +3,25 @@ Created on 15.06.2018
 
 @author: nicobucher
 '''
-import unittest
 from hangman import hangman
-from wheel.signatures import assertTrue
+import pytest
 
+@pytest.fixture()
+def hangman_game():
+    game = hangman.HangmanGame('test')
+    return game
 
-class TestHangman(unittest.TestCase):
+def test_init(hangman_game):
+    assert hangman_game._chars != 'abcdefghijklmnopqrstuvwxyz'
 
-
-    def test_init(self):
-        game = hangman.HangmanGame('test')
-        self.assertNotEqual(game._chars,'abcdefghijklmnopqrstuvwxyz')
-
-    def test_answer_correct(self):
-        game = hangman.HangmanGame('test')
-        game._last_answer = '_____'
-        self.assertTrue(game.check_answer('__T__'))
-        
-    def test_answer_incorrect(self):
-        game = hangman.HangmanGame('test')
-        game._last_answer = '__T__'
-        self.assertFalse(game.check_answer('__T__'))
-        
-        
-if __name__ == '__main__':
-    #import sys;sys.argv = ['', 'Test.testName']
-    unittest.main()
+# '____' and 'BBBBBB' are a problem, the length of the searched word should not change.
+# Also a previously identified character should not change.
+# This is not yet detected by the current implementation
+@pytest.mark.parametrize('answer', ['____', '_____', 'AA_AA', 'BBBBBB'])
+def test_answer_correct(hangman_game, answer):
+    hangman_game._last_answer = answer
+    assert hangman_game.check_answer('__T__')
+    
+def test_answer_incorrect(hangman_game):
+    hangman_game._last_answer = '__T__'
+    assert hangman_game.check_answer('__T__') == False
